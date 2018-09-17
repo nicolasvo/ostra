162b1d3562dcd30afb87d4fc1656c49171c8a416
Nicolas <nicolasvo95@gmail.com>
2018-09-17 11:07:00 +0200

    add magiclogger

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,7 +1,9 @@
 from app.api.models import Data
 from apistar import http
 from googletrans import Translator
+from magic_logger import MagicLogger
 
+logger = MagicLogger("Oyster")
 
 def welcome(name=None):
     if name is None:
@@ -12,6 +14,7 @@ def welcome(name=None):
 def get_translation(word, language_destination="en", language_source="ru"):
     translator = Translator()
     word_translated = translator.translate(word, dest=language_destination, src=language_source).text
+    logger.debug(f"{word} > {word_translated}")
 
     return word_translated
 
