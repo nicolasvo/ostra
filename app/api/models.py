8a2b914cba6b3c8754b74c05431e6cd7d2948dd6
nicolasvo <nicolasvo95@gmail.com>
2019-04-08 01:17:08 +0200

    add gspread

--- app/api/models.py
+++
@@ -1,8 +0,0 @@
-from apistar import types
-from apistar.validators import String
-
-
-class Data(types.Type):
-    word = String(min_length=1,
-                  description="Word to translate (from russian to english)",
-                  allow_null=False)
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gunicorn --timeout 100 --bind 0.0.0.0:${PORT} wsgi_app:app
