996b7425bb7cd0062947d3771f3ed78421715cc0
Nicolas Vo <nivo@octo.com>
2019-04-09 16:30:01 +0200

    add row offset

--- api/index.py
+++ api/index.py
@@ -19,6 +19,9 @@ class Translator():
         self.map_language = {index + 1: language for index, language in enumerate(sheet.row_values(1))}
         self.to_languages = [{"language": language, "index": index} for index, language in self.map_language.items()]
 
+    def parse_parameter(self, parameters):
+        return {k: int(v) if k in ["row", "col"] else v for k, v in parameters.items()}
+
     def show(self):
         list_all = self.sheet.get_all_records()
         return list_all
@@ -28,7 +31,7 @@ class Translator():
         return len(list_all)
 
     def update_(self, row, col, word):
-        self.sheet.update_cell(row, col, word)
+        self.sheet.update_cell(row + 1, col, word)
 
     def translate(self, row, col, word, from_language, to_language):
         content = f'=GOOGLETRANSLATE("{word}", "{from_language}", "{to_language["language"]}")'
@@ -61,18 +64,24 @@ def hello_world():
 @app.route('/words', methods=['POST'])
 def insert():
     body = request.get_json()
-    map_parameter = ["word", "col"]
+    map_parameter = ["col", "word"]
     parameters = {parameter: body[parameter] for parameter in map_parameter}
-    translator.insert_(int(parameters["col"]), parameters["word"])
+    parameters = translator.parse_parameter(parameters)
+    translator.insert_(**parameters)
     return "", 201
 
 @app.route("/words", methods=["PUT"])
 def update():
     body = request.get_json()
-    map_parameter = ["word", "row", "col"]
+    map_parameter = ["row", "col", "word"]
     parameters = {parameter: body[parameter] for parameter in map_parameter}
-    translator.update_(int(parameters["row"]), int(parameters["col"]), parameters["word"])
-    return "", 201
+    parameters = translator.parse_parameter(parameters)
+
+    if parameters["row"] <= 0:
+        return "Row must be greater than 0.", 400
+    else:
+        translator.update_(**parameters)
+        return "", 201
 
 @app.route("/words", methods=["DELETE"])
 def delete():
