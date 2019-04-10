51bd8bdd2944bce07c7f5dba29b4948f8de96f06
nicolasvo <nicolasvo95@gmail.com>
2019-04-10 20:07:25 +0200

    add token expiration check

--- api/index.py
+++ api/index.py
@@ -9,19 +9,33 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('api/res/contract.json', scope)
-client = gspread.authorize(creds)
+# scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+# creds = ServiceAccountCredentials.from_json_keyfile_name('api/res/contract.json', scope)
+# client = gspread.authorize(creds)
+# sheet = client.open("dictionary").sheet1
 
-sheet = client.open("dictionary").sheet1
 
+class Sheet():
 
-class Translator():
+    def __init__(self, name):
+        self.name = name
+        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        self.creds = ServiceAccountCredentials.from_json_keyfile_name('api/res/contract.json', scope)
+        self.client = gspread.authorize(self.creds)
 
-    def __init__(self, sheet):
-        self.sheet = sheet
-        self.map_language = {index + 1: language for index, language in enumerate(sheet.row_values(1))}
+    @property
+    def sheet(self):
+        if self.creds.access_token_expired:
+            self.client.login()
+        return self.client.open(self.name).sheet1
+
+    # @property
+    def map_language(self):
+        self.map_language = {index + 1: language for index, language in enumerate(self.sheet.row_values(1))}
         self.to_languages = [{"language": language, "index": index} for index, language in self.map_language.items()]
+
+    def refresh_token(self):
+        self.client.login()
 
     def parse_parameter(self, parameters):
         return {k: int(v) if k in ["row", "col"] else v for k, v in parameters.items()}
@@ -58,36 +72,43 @@ class Translator():
     def delete_row_(self, row):
         self.sheet.delete_row(row + 1)
 
-translator = Translator(sheet)
+
+sheet = Sheet("dictionary")
+sheet.map_language()
+
 
 @app.route("/")
 def hello_world():
-    print(translator.show())
-    return jsonify(translator.show())
+    print(sheet.show())
+    return jsonify(sheet.show())
+
 
 @app.route('/words', methods=['POST'])
 def insert():
-    parameters = translator.parse_parameter(request.get_json())
-    translator.insert_(**parameters)
+    parameters = sheet.parse_parameter(request.get_json())
+    sheet.insert_(**parameters)
     return "", 201
+
 
 @app.route("/words", methods=["PUT"])
 def update():
-    parameters = translator.parse_parameter(request.get_json())
+    parameters = sheet.parse_parameter(request.get_json())
     if parameters["row"] <= 0:
         return "Row must be greater than 0.", 400
     else:
-        translator.update_(**parameters)
+        sheet.update_(**parameters)
         return "", 201
+
 
 @app.route("/words", methods=["DELETE"])
 def delete():
-    parameters = translator.parse_parameter(request.get_json())
+    parameters = sheet.parse_parameter(request.get_json())
     if parameters["row"] <= 0:
         return "Row must be greater than 0.", 400
     else:
-        translator.delete_row_(**parameters)
+        sheet.delete_row_(**parameters)
         return "", 204
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT, debug=True)
